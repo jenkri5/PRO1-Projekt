3f99ff31c69ad22cc0e3e8fb2e4ecb832d7df455
jenkri5 <jenskristensen@me.com>
2022-03-14 23:27:13 +0100

    nested dies and cleanup

--- L00 Other/src/main/java/yahtzee/Game.java
+++ L00 Other/src/main/java/yahtzee/Game.java
@@ -4,23 +4,24 @@ public class Game {
 
 	private final int[] PLAYERS;
 	private final int[][] BOARD;
-	private final DiceThrower DICE_THROWER = new DiceThrower(5);
+	private final DiceThrower DICE_THROWER;
 	private int m_PlayerTurn;
 	private boolean GAME_OVER;
-	
-	public Game(int nPlayers) {
+
+	public Game(int nPlayers, int nDice) {
 		PLAYERS = new int[nPlayers];
-		BOARD = new int[LowerSection.values().length+UpperSection.values().length][nPlayers];
+		BOARD = new int[LowerSection.values().length + UpperSection.values().length][nPlayers];
+		DICE_THROWER = new DiceThrower(nDice);
 		m_PlayerTurn = 0;
 	}
 
 	public void initialize() {
-		for(int i = 0; i < PLAYERS.length; i++)
+		for (int i = 0; i < PLAYERS.length; i++)
 			PLAYERS[i] = i;
 	}
 
 	public void run() {
-		while(!GAME_OVER) {
+		while (!GAME_OVER) {
 			// GAME LOGIC
 			DICE_THROWER.roll();
 			DICE_THROWER.print();
@@ -38,20 +39,19 @@ public class Game {
 
 	public void printBoard() {
 		for (int[] scores : BOARD) {
-			for (int score : scores) {
+			for (int score : scores)
 				System.out.print(score);
-			}
 			System.out.println();
 		}
 	}
 
 	public void incrementPlayerTurn() {
-		if (m_PlayerTurn >= PLAYERS.length-1)
+		if (m_PlayerTurn >= PLAYERS.length - 1)
 			m_PlayerTurn = 0;
 		else
 			m_PlayerTurn++;
 	}
-	
+
 	public void setScore(int section, int player, int score) {
 		BOARD[section][player] = score;
 	}
